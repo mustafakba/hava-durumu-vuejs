7ecd59bd6de0d590d6b08dc1823358eacc2e46e5
mustafakba <kaba.mustafaa@gmail.com>
2022-10-18 16:12:15 +0300

    Header Ve Card Component olusturuldu

---
+++ src/components/CardComp.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center">
+        <b-card
+            class="bg-light"
+            style="max-width: 20rem;"
+
+        >
+            <b-card-text>
+                <h2>{{this.$store.state.cityName}},  {{this.$store.state.newTemp}}°C</h2>
+                <h5>Hava Durumu : {{this.$store.state.data.weather}}</h5>
+                <span>Boylam : {{this.$store.state.data.coord.lon}} <br></span>
+                <span>Enlem : {{this.$store.state.data.coord.lat}}</span>
+
+<!--                <h5>Sıcaklık: {{}}</h5>-->
+<!--                <h5>Hissedilen Sıcaklık: {{}}</h5>-->
+<!--                <h5>Max Sıcaklık: {{}}</h5>-->
+<!--                <h5>Min Sıcaklık: {{}}</h5>-->
+<!--                <h5>Görünürlük: {{}}</h5>-->
+<!--                <h5>Bulut Oranı: {{}}</h5>-->
+
+            </b-card-text>
+
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CardComp"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="py-5 d-flex flex-column justify-content-center align-items-center text-white">
+    <h3>Güncel Hava Durumu</h3>
+    <b-form-input class=" w-25"  placeholder="Enter city name"></b-form-input>
+</div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComp"
+}
+</script>
+
+<style scoped>
+.background{
+}
+</style>
