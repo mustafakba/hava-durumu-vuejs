d5457c308154631df7fe138a22da98e9f2f6d681
mustafakba <kaba.mustafaa@gmail.com>
2022-10-18 11:57:33 +0300

    firstCommit

---
+++ src/state.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import vuex from 'vuex';
+
+Vue.use(vuex);
+
+const store = new Vuex.Store({
+    state:{
+        apiKey : '0eb69af3ffadfd2a436f6cfb5bf10e75'
+    },
+    getters:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    }
+});
+
+export default store ;
