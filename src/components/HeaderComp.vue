ca1e6ea4badf40426451d9abd0409f3bb67d486b
mustafakba <kaba.mustafaa@gmail.com>
2022-10-19 15:17:54 +0300

    -Header düzenlendi

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,14 +1,22 @@
 <template>
 <div class="py-5 d-flex flex-column justify-content-center align-items-center text-white">
-    <h3>Güncel Hava Durumu</h3>
-    <b-form-input class=" w-25"  placeholder="Enter city name"></b-form-input>
+    <h3>Hava Durumu</h3>
+    <b-form-input class=" w-25" @keyup.enter="searchWeather" placeholder="Enter city name"></b-form-input>
 </div>
 </template>
 
 <script>
 export default {
-    name: "HeaderComp"
+    name: "HeaderComp",
+    methods:{
+        searchWeather(e){
+            this.$store.state.cityName = e.target.value
+            e.target.value = ''
+            this.$store.dispatch("getRequest")
+        }
+    }
 }
+
 </script>
 
 <style scoped>
