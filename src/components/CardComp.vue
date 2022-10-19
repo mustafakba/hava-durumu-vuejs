c089b55945f079a3dc21b396e5526e1572199855
mustafakba <kaba.mustafaa@gmail.com>
2022-10-19 15:18:33 +0300

    -inputa girilen veri alındı

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -7,9 +7,11 @@
         >
             <b-card-text>
                 <h2>{{this.$store.state.cityName}},  {{this.$store.state.newTemp}}°C</h2>
-                <h5>Hava Durumu : {{this.$store.state.data.weather}}</h5>
-                <span>Boylam : {{this.$store.state.data.coord.lon}} <br></span>
-                <span>Enlem : {{this.$store.state.data.coord.lat}}</span>
+                <h5>Hava Durumu : {{this.$store.state.data.weather ? this.$store.state.data.weather[0].main : this.$store.state.data.weather}}</h5>
+                <div>Hissedilen Sıcaklık : {{this.$store.state.new_feels_like}}°C</div>
+                <span>Boylam : {{this.$store.state.coord_lon}} <br></span>
+                <span>Enlem : {{this.$store.state.coord_lat}}</span>
+                <div>Max Görünürlük : {{this.$store.state.data.visibility}}m /10km</div>
 
 <!--                <h5>Sıcaklık: {{}}</h5>-->
 <!--                <h5>Hissedilen Sıcaklık: {{}}</h5>-->
